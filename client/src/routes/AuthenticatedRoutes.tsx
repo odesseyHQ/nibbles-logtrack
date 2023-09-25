ac11a6cfd3cffc503afe88214c0f64ff834f673b
aghilodessey <aghil@odesseylabs.com>
2023-09-25 10:00:36 +0530

    logdetails prototype

--- client/src/routes/AuthenticatedRoutes.tsx
+++ client/src/routes/AuthenticatedRoutes.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Projects from "../pages/projects/Projects";
 import Logs from "../pages/logs/Logs";
 import ProjectsList from "../pages/projects/ProjectsList";
+import LogDetails from "../pages/logs/LogDetails";
 
 const AuthenticatedRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AuthenticatedRoutes = () => {
       <Routes>
         <Route path="/logs" element={<Logs />} />
         <Route path="/projectslist" element={<ProjectsList />} />
+        <Route path="/logdetails/:id" element={<LogDetails />} />
       </Routes>
     </div>
   );
