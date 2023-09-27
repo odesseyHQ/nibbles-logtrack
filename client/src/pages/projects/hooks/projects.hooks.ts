78157aac62234fd07a8151d4bc88561b7e600426
aghilodessey <aghil@odesseylabs.com>
2023-09-27 17:34:11 +0530

    Create and projectlist api integration done

---
+++ client/src/pages/projects/hooks/projects.hooks.ts
@@ -0,0 +1,87 @@
+import { useToast } from "@chakra-ui/react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+import axios from "axios";
+
+const fetchProjectList = async () => {
+  const url = "http://logger.odesseylabs.com/api/project";
+  const requestBody = {
+    filter: {},
+    sort: {
+      sortKey: "projectId",
+      sortOrder: "ASC",
+    },
+    limit: 20,
+    offset: 0,
+  };
+
+  const headers = {
+    Authorization: "Bearer NIBBLES_ODESSEY",
+    "Content-Type": "application/json",
+  };
+
+  try {
+    const response = await axios.post(url, requestBody, { headers });
+    return response.data.data;
+  } catch (error) {
+    throw new Error("Failed to fetch project list");
+  }
+};
+export const useProjectList = () => {
+  return useQuery(["projectList"], fetchProjectList);
+};
+
+export const useCreateProjectForm = (
+  onSuccessCallBack?: VoidFunction,
+  onErrorCallBack?: VoidFunction
+) => {
+  const toast = useToast();
+
+  const CREATE_PROJECT = `http://logger.odesseylabs.com/api/project/create`;
+  const headers = {
+    Authorization: "Bearer NIBBLES_ODESSEY",
+    "Content-Type": "application/json",
+  };
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (_payload) => {
+      const res = axios
+        .post(CREATE_PROJECT, _payload, { headers })
+        .then((res: any) => {
+          return res.data;
+        })
+        .catch((err: any) => {
+          throw new Error(`error occurred => ${JSON.stringify(err)}`);
+        });
+      return res;
+    },
+    onSuccess: (response: any) => {
+      if (onSuccessCallBack) {
+        onSuccessCallBack();
+      } else {
+        toast({
+          title: "Success",
+          position: "top-right",
+          isClosable: true,
+          status: "success",
+          duration: 3000,
+        });
+      }
+      queryClient.invalidateQueries({ queryKey: ["projectList"] });
+    },
+
+    onError: (error: any) => {
+      onErrorCallBack
+        ? onErrorCallBack()
+        : toast({
+            title: "Error",
+            position: "top-right",
+            isClosable: true,
+            status: "error",
+            duration: 3000,
+          });
+      console.error("API POST **failed, error ->", error);
+    },
+    retry: 0,
+  });
+};
